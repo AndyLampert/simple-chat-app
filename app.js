df79ae4463063c6ad3f4026a617834c0edd56f81
AndyLampert <lamperta@gmail.com>
2014-07-06 16:43:04 -0600

    beginning layout

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
-var app = express();
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+io.on('connection', function(){ /* … */ });
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/my_database');
+
+// get notified if we connect successfully or if a connection error occurs
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+  // yay!
+});
+
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
-app.use(bodyParser());
 
 app.get('/', function(req, res) {
 	res.render('index');
 });
 
-var server = app.listen(9634, function() {
-	console.log('Express server listening on port ' + server.address().port);
+server.listen(3000, function(){
+	console.log("Listening on port 3000");
 });
---
+++ public/js/main.js
@@ -0,0 +1,7 @@
+// this is an example notification
+// param1 - title of the notification (string)
+// param2 - object of settings (optional)
+var notification = new Notification('Email received', {
+  body: 'You have a total of 3 unread emails'
+});
+
